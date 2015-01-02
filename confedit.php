5c7b972073e69e5ad05d8773bfaee5b87f05c9e7
ponbiki <ponbiki@beryllium>
2015-01-02 02:43:43 -0500

    hostname delete and add forms

--- confedit.php
+++ confedit.php
@@ -18,14 +18,14 @@ if (!($con = ssh2_connect($server, $port))) {
     if (!ssh2_auth_password($con, $ssh_user, $ssh_pass)) {
         die('Failed to authenticate');
     } else {
-        if (!($inifile = ssh2_scp_recv($con, $_SESSION['confpath'], '/tmp/'.$_SESSION['conffile'] ))) {
+        if (!($inifile = ssh2_scp_recv($con, $_SESSION['confpath'], 'tmp/'.$_SESSION['conffile'] ))) {
             die('Unable to get file');
         }
         fclose($stream);
     }
 }
 
-$ini_array = parse_ini_file('/tmp/'.$_SESSION['conffile'], true);
+$ini_array = parse_ini_file('tmp/'.$_SESSION['conffile'], true);
 
 foreach ($ini_array as $category => $value) {
     if ($category == "hostname") {
@@ -39,14 +39,14 @@ foreach ($ini_array as $category => $value) {
     <table style="float:left;">
 <?php
 foreach ($domains as $domain) {
-    echo <<<_END
+    //echo <<<_END ?>
         <tr>
-            <td><label><span style="float:left;">$domain</span>
+            <td><label><span style="float:left;"><?php echo $domain ?></span>
             <span style="float:right;">
-                <input type="radio" name="deldomain" value="$domain" /></span>
+                <input type="radio" name="deldomain" value="<?php echo $domain ?>" /></span>
             </label></td>
-        </tr>    
-_END;
+        </tr> <?php
+//_END;
 }
 ?>
         <tr>
@@ -54,6 +54,48 @@ _END;
                     <span style="float:right;"><input type="submit" value="Remove" /></span>
                 </label></td>
         </tr>
+    </table>
+</form>
+
+<form method='post' action='domainadd.php'>
+    <table style='float:right;'>
+        <tr>
+            <td>
+                <label>
+                    <span style='float:left;'>
+                        Add a new hostname
+                    </span>
+                </label>
+            </td>
+            <td>
+                <label>
+                    <span style='float:right;'>
+                        <input type='text' maxlength='24' name='newhost' value="" />
+                    </span>
+                </label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label>
+                    <span style='float:left;'>
+                        <?php
+                        if (isset($_SESSION['hostadderror'])) {
+                            echo $_SESSION['hostadderror'];
+                        }
+                        ?>
+                    </span>
+                </label>
+            </td>
+            <td>
+                <label>
+                    <span style='float:right;'>
+                        <input type="submit" value="Add" />
+                    </span>
+                </label>
+            </td>
+        </tr>
+
     </table>
 </form>
 <?php
